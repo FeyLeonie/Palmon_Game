b4d66f0c1bb36b797a74b453dce9ca73e07f80fe
i589533 <feyschreier@outlook.com>
2024-05-18 11:36:20 +0200

    neuster Stand

--- src/Gameplay/Game.java
+++ src/Gameplay/Game.java
@@ -64,7 +64,7 @@ public class Game extends Printing
 
             if(answer != 1 && answer != 2)
             {
-                print("Please type in (1) for yes and (2) for noo.");
+                print("Please type in (1) for yes and (2) for no.");
             }
         }
         while(answer != 1 && answer != 2);
